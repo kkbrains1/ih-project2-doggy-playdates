041c2e58be4974ba45ac8d78785b22b7db372525
cristianekoshino <cristianekoshino@gmail.com>
2020-05-18 14:29:29 +0100

    unique property added in user

--- models/user.js
+++ models/user.js
@@ -11,7 +11,8 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     lowercase: true,
-    trim: true
+    trim: true,
+    unique: true
   },
   passwordHash: {
     type: String
